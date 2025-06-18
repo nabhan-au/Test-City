e86ef76110fcde1de9fda0ed3c76456f311fd654
nabhan-au <nabhan.au@gmail.com>
2025-06-19 01:34:11 +0900

    update blockExtractor

--- block-extractor/src/main/java/org/example/BlockExtractor.java
+++ block-extractor/src/main/java/org/example/BlockExtractor.java
@@ -2,22 +2,24 @@ package org.example;
 
 import org.pitest.classinfo.ClassName;
 import org.pitest.classpath.*;
+import org.pitest.coverage.BlockLocation;
 import org.pitest.coverage.analysis.LineMapper;
 import org.pitest.functional.prelude.Prelude;
 import org.pitest.mutationtest.config.DefaultCodePathPredicate;
 import org.pitest.mutationtest.config.DefaultDependencyPathPredicate;
 import org.pitest.util.Glob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class BlockExtractor {
+
+    private static final Logger logger = LoggerFactory.getLogger(BlockExtractor.class);
 
     private final Collection<File> compiledCodeDirectories;
 
@@ -64,12 +66,14 @@ public class BlockExtractor {
                 .collect(Collectors.toList()));
     }
 
-    public void process() {
+    public List<Map<BlockLocation, Set<Integer>>> process() {
+        List<Map<BlockLocation, Set<Integer>>> result = new ArrayList<>();
         ProjectClassPaths classPaths = this.createProjectClassPaths();
         DefaultCodeSource source = new DefaultCodeSource(classPaths);
         LineMapper lineMapper = new LineMapper(source);
         for (ClassName clazz : source.getAllClassAndTestNames()) {
-            System.out.println(lineMapper.mapLines(clazz));
+            result.add(lineMapper.mapLines(clazz));
         }
+        return result;
     }
 }
--- block-extractor/src/main/java/org/example/Main.java
+++ block-extractor/src/main/java/org/example/Main.java
@@ -1,25 +1,15 @@
 package org.example;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.io.File;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args) throws Exception {
-        if (args.length < 1) {
-            System.err.println("Usage: java -cp <classpath> org.example.BlockExtractor <directory_path>");
-            System.exit(1);
-        }
-
-        String dirPath = args[0];
-        File root = new File(dirPath);
-
-        if (!root.exists() || !root.isDirectory()) {
-            System.err.println("Provided path is not a valid directory: " + dirPath);
-            System.exit(1);
-        }
-
-        BlockExtractor block = new BlockExtractor(root);
-        block.process();
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
     }
 }
