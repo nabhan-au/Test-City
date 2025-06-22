efbe620cddc09ccef3627ac11a76245d120200ab
ParnThanatibordee <parn8946@gmail.com>
2025-06-23 00:45:19 +0700

    extract loaders

--- visualizer/data/loaders.js
+++ visualizer/data/loaders.js
@@ -1,27 +1,43 @@
-import {loadJson} from "./helpers";
+import { loadJson } from "./helpers";
 
 const backendUrl = "http://localhost:8000"
 
-export function complexityExample (d3, example) {
+export function complexityExample(d3, example) {
     const data = loadJson(d3, '../data/complexity/' + example + '_complexity.json');
     return data;
 }
 
-export function fetchProjectData  (projectName) {
-    const apiUrl = `${backendUrl}/coverall/project/${projectName}`;
+export function fetchProjectList() {
+    const url = `${backendUrl}/coverall/project/list`;
+    return fetch(url)
+      .then(res => {
+        if (!res.ok) throw new Error(`Error fetching project list: ${res.statusText}`);
+        return res.json();
+      });
+  }
 
-    return fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            return data.summary;
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-            throw error;
+export function uploadProject(projectName, files) {
+    const url = `${backendUrl}/coverall/project/${projectName}`;
+    const formData = new FormData();
+    for (const f of files) {
+        formData.append("files", f, f.webkitRelativePath);
+    }
+    return fetch(url, {
+        method: "POST",
+        body: formData,
+    })
+        .then(res => {
+            if (!res.ok) throw new Error(`Upload failed: ${res.statusText}`);
+            return res.json();
         });
+}
+
+export function fetchProjectData(projectName) {
+    const url = `${backendUrl}/coverall/project/${projectName}`;
+    return fetch(url)
+        .then(res => {
+            if (!res.ok) throw new Error("Network response was not ok");
+            return res.json();
+        })
+        .then(data => data.summary);
 }
