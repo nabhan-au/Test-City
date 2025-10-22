4ff9212d943a00d663ca138edc4d2d404311d129
nabhan-au <nabhan.au@gmail.com>
2025-10-23 01:08:11 +0900

    Remove unnecessary code

--- data_collector/models/coverall_download_error.py
+++
@@ -1,7 +0,0 @@
-class CoverallDownloadError(Exception):
-
-    def __int__(self, message):
-        self.__message = message
-
-    def __str__(self):
-        return self.__message
--- data_collector/models/github_clone_repo_error.py
+++
@@ -1,7 +0,0 @@
-class GithubCloneRepoError(Exception):
-
-    def __int__(self, message):
-        self.__message = message
-
-    def __str__(self):
-        return self.__message
